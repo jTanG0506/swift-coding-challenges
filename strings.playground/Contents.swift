2061e14a25685b32e2ef20addb94a3aefef18342
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-03-16 22:28:20 +0000

    Challenge 2 - Is a string a palindrome?

--- strings.playground/Contents.swift
+++ strings.playground/Contents.swift
@@ -10,3 +10,15 @@ assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
 assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
 assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
 assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
+
+// Challenge 2
+// Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.
+func challenge2(input: String) -> Bool {
+  return input.lowercased() == String(input.lowercased().reversed())
+}
+
+assert(challenge2(input: "rotator") == true, "Challenge 2 failed")
+assert(challenge2(input: "Rats live on no evil star") == true, "Challenge 2 failed")
+assert(challenge2(input: "Never odd or even") == false, "Challenge 2 failed")
+assert(challenge2(input: "Hello, world") == false, "Challenge 2 failed")
+
